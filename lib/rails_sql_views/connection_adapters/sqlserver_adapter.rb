b2c6ec6923a9088cc53904fbca80218b48fa944a
mereghost <marcello.rocha@gmail.com>
2011-12-05 18:04:35 -0200

    Adjustments made for our SQLServer setup Signed-off-by: mereghost <marcello.rocha@gmail.com>

--- lib/rails_sql_views/connection_adapters/sqlserver_adapter.rb
+++ lib/rails_sql_views/connection_adapters/sqlserver_adapter.rb
@@ -5,35 +5,40 @@ module RailsSqlViews
       def supports_views?
         true
       end
-      
+
+      def supports_drop_table_cascade?
+        return false
+      end
+
+
       # Get all of the non-view tables from the currently connected schema
       def base_tables(name = nil)
         # this is untested
-        select_values("SELECT table_name FROM information_schema.tables", name)
+        select_values("SELECT table_name FROM information_schema.tables WHERE TABLE_TYPE <> 'VIEWS'", name)
       end
       alias nonview_tables base_tables
-      
+
       # Returns all the view names from the currently connected schema.
       def views(name = nil)
-        select_values("SELECT table_name FROM information_schema.views", name)
+        select_values("SELECT table_name FROM information_schema.views WHERE TABLE_TYPE = 'VIEWS'", name)
       end
-      
+
       # Get the view select statement for the specified view.
       def view_select_statement(view, name=nil)
         q =<<-ENDSQL
           SELECT view_definition FROM information_schema.views
           WHERE table_name = '#{view}'
         ENDSQL
-        
+
         view_def = select_value(q, name)
-        
+
         if view_def
           return convert_statement(view_def)
         else
           raise "No view called #{view} found"
         end
       end
-      
+
       private
       def convert_statement(s)
         s.sub(/^CREATE.* AS (select .*)/i, '\1').gsub(/\n/, '')
@@ -41,3 +46,4 @@ module RailsSqlViews
     end
   end
 end
+
