14558c385a36e01ced98a53ff2d1ff3b15db7d0f
mereghost <marcello.rocha@gmail.com>
2012-09-28 14:28:27 -0300

    Removed the disable_referential_integrity since it was causing a stack too deep error AND I don't need it

--- lib/rails_sql_views/connection_adapters/abstract_adapter.rb
+++ lib/rails_sql_views/connection_adapters/abstract_adapter.rb
@@ -2,7 +2,7 @@ module RailsSqlViews
   module ConnectionAdapters
     module AbstractAdapter
       def self.included(base)
-        base.alias_method_chain :disable_referential_integrity, :views_excluded
+        #base.alias_method_chain :disable_referential_integrity, :views_excluded
       end
 
       # Subclasses should override and return true if they support views.
@@ -14,24 +14,24 @@ module RailsSqlViews
       def supports_drop_table_cascade?
         return true
       end
-      
-      def disable_referential_integrity_with_views_excluded(&block)
-        self.class.send(:alias_method, :original_tables_method, :tables)
-        self.class.send(:alias_method, :tables, :base_tables)
-        disable_referential_integrity_without_views_excluded(&block)
-      ensure
-        self.class.send(:alias_method, :tables, :original_tables_method)
-      end
-      
+
+      #def disable_referential_integrity_with_views_excluded(&block)
+      #  self.class.send(:alias_method, :original_tables_method, :tables)
+      #  self.class.send(:alias_method, :tables, :base_tables)
+      #  disable_referential_integrity_without_views_excluded(&block)
+      #ensure
+      #  self.class.send(:alias_method, :tables, :original_tables_method)
+      #end
+
       def supports_view_columns_definition?
         true
       end
-      
+
       # Get a list of all views for the current database
       def views(name = nil)
         raise NotImplementedError, "views is an abstract method"
       end
-      
+
       # Get the select statement for the specified view
       def view_select_statement(view, name=nil)
         raise NotImplementedError, "view_select_statement is an abstract method"
